ef3dfcb3fab9b5a85e601f54e8aaf42c5a54656c
jonasbarth <jb901@exeter.ac.uk>
2020-05-27 18:13:57 +0200

    Added food

--- env.py
+++ env.py
@@ -2,8 +2,9 @@
 
 
 import turtle
-import time
+import random
 from snake import Snake
+from food import Food
 
 
 class SnakeEnv:
@@ -12,6 +13,10 @@ class SnakeEnv:
         self.wn = turtle.Screen() 
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.current_food = None
+        self.snake = Snake()
+        self.actions = {0: "up", 1: "right", 2: "down", 3: "left"}
+        self.points = 0
         
         
 
@@ -19,19 +24,38 @@ class SnakeEnv:
         self.wn.title("Snake Game")
         self.wn.bgcolor("white")
         self.wn.setup(width=self.screen_width, height=self.screen_height)
+        self.__generate_food()
         #self.wn.tracer(0)
 
       
-
-
     def step(self, action):
-        snake = Snake()
-        snake.set_direction("up")
+        
+        direction = self.actions[action]
 
-        while True:
-            self.wn.update()
-            time.sleep(0.1)
-            #print(snake.head.direction)
-            snake.up()
+        self.snake.move(direction)
+       
+        self.wn.update()
 
-        self.wn.mainloop()
+        if self.__touch_food():
+            self.points += 1
+            self.__generate_food()
+
+    
+
+    def __generate_food(self):
+        x_start = (-1) * self.screen_width / 2
+        x_end = self.screen_width / 2
+        x_rand = random.randrange(x_start, x_end)
+
+        y_start = (-1) * self.screen_height / 2
+        y_end = self.screen_height / 2
+        y_rand = random.randrange(y_start, y_end)
+
+        self.current_food = Food(x_rand, y_rand)
+
+
+    def __touch_food(self):
+        if self.snake.food_distance(self.current_food) < 20:
+            return True
+        return False
+
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from env import SnakeEnv
+import time
 
 
 env = SnakeEnv(600, 600)
 env.reset()
 
-env.step(1)
+
+for n in range(10):
+    env.step(1)
+   
+
+
+env.wn.mainloop()
