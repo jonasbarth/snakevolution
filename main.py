b93f989d7bcb93154acf04c6110981a9387a1246
jonasbarth <jb901@exeter.ac.uk>
2020-05-30 14:16:24 +0200

    added tail logic

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from env import SnakeEnv
 import time
-from human_agent import Human_Agent
+from human_agent import HumanAgent
 
 
 env = SnakeEnv(600, 600)
+env.reset()
+#agent = HumanAgent(env)
+#agent.play()
 
-agent = HumanAgent(env)
-
-agent.play()
-   
+env.step(1)
+env.snake.add_tail()
+env.step(1)
+env.snake.add_tail()
+env.step(1)
 
 
 env.wn.mainloop()
