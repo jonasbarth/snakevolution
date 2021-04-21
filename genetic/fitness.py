2502bbb6baa2860010bcf108bdf1f3817a99dd41
jonasbarth <jb901@exeter.ac.uk>
2021-04-21 20:15:40 +0200

    [MS-3] Create fitness function
    * Create fitness function for maximising eating food.

--- genetic/fitness.py
+++ genetic/fitness.py
@@ -5,5 +5,10 @@ def maximise_moves(env: Env) -> float:
     return env.total_steps()
 
 
+def maximise_food_eaten(env: Env) -> float:
+    return env.total_points()
+
+
 fitness_func_mapping = dict()
 fitness_func_mapping["MAXIMISE_MOVES"] = maximise_moves
+fitness_func_mapping["MAXIMISE_FOOD_EATEN"] = maximise_food_eaten
