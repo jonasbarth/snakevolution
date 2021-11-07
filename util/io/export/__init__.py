46def2e68464e4938f8bae6eed13da23fe7f7259
jonasbarth <jb901@exeter.ac.uk>
2021-11-07 14:44:14 +0100

    [MS-57] update init.py
    * update init.pys to make imports easier

--- util/io/export/__init__.py
+++ util/io/export/__init__.py
@@ -1,4 +1,5 @@
 from util.io.export.game_sequence_exporter import GameSequenceExporter
 from util.io.export.genetic_exporter import GeneticExporter
+from util.io.export.genetic_exporter import GeneticPopulationData
 from util.io.export.hyper_parameter_exporter import HyperParameterExporter
 from util.io.export.exporter import Exporter
